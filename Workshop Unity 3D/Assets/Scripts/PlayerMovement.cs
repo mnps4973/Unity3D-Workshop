11a155055986f0c49e6b66bce464e0322cc098e6
Margarida <56021765+mnps4973@users.noreply.github.com>
2024-10-23 18:37:10 +0100

    unity workshop base added

---
+++ Workshop Unity 3D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody _rb;
+    private float speed = 2.0f;
+    Vector3 m_Movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalValue = -(Input.GetAxisRaw("Vertical"));
+        float verticalValue = Input.GetAxisRaw("Horizontal");
+
+        m_Movement.Set(horizontalValue, 0f, verticalValue);
+        if (horizontalValue != 0 && verticalValue != 0) {
+            m_Movement.Normalize();
+		}
+
+        Vector3 deltaMovement = m_Movement * speed * Time.deltaTime;
+        _rb.MovePosition(transform.position + deltaMovement);
+    }
+}
